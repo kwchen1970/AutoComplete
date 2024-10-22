a04ebe2728322b20b7879276147cca090e135e6d
xt73 <xt73@cornell.edu>
2024-10-22 00:21:01 -0400

    [search] can now get all leaves that match the given prefix.

--- lib/trie.ml
+++ lib/trie.ml
@@ -1,33 +1,13 @@
 (* Implementation of Trie Trees, due 11/5.*)
 open Str
 
-(* module type TrieTree = sig type word (** The type of an element in the Trie
-   tree (most likely sequences of chars). *)
-
-   type t (** The type of a Trie tree. *)
-
-   exception Empty (** Raised when an operation cannot be performed because a
-   Trie tree is empty. *)
-
-   val empty : t (** [empty] is the empty Trie tree *)
-
-   (* val is_empty : t -> bool *) (** [is_empty q] is [true] when [q] is the
-   empty Trie tree and [false] otherwise. *)
-
-   val find : char -> t -> string
-
-   val insert : word -> t -> t (** [insert word tree] is the Trie tree that
-   results from inserting [word] into [tree]. *)
-
-   val to_word_list : t -> word list (** [to_word_list tree] returns a list
-   containing all the word leaves in the Trie tree [tree]. *) end *)
-
 (* module MakeTrieTree (TR : TrieTree) = struct *)
 (* [elt] is the type of an element in the priority queue. *)
+
 module CharMap = Map.Make (Char)
 
 (* Define the type of CharMap where the values are of type value_type *)
-type t = Node of bool * t CharMap.t
+type t = Node of t CharMap.t
 
 (* [t] is the representation type for the Trie tree, the tree is a CharMap with
    keys being the characters 'a' though 'z' and each key character pointing to
@@ -35,7 +15,7 @@ type t = Node of bool * t CharMap.t
 
 exception Empty
 
-let empty = Node (false, CharMap.empty)
+let empty = Node CharMap.empty
 
 let is_empty tree =
   match CharMap.cardinal tree with
@@ -44,16 +24,16 @@ let is_empty tree =
 (* [empty] is the empty Trie tree, represented by an array containing 26 Nodes
    with elements [word] = "a" to "z" and both [t] = Leaf. *)
 
-let get_tree (Node (is_word, tree)) = tree
+let get_tree (Node tree) = tree
 
 let to_char_list word =
   List.of_seq (String.to_seq (String.lowercase_ascii word))
 
 (* [insert char_list tree] is a function that inserts the word represented by
    [char_list] into the Trie tree [tree]. *)
-let rec insert char_list (Node (is_word, tree)) =
+let rec insert char_list (Node tree) =
   match char_list with
-  | [] -> Node (true, tree)
+  | [] -> Node tree
   | h :: t ->
       let new_tree =
         (* If [h] is a key in [tree], then traverse the tree that [h] points
@@ -64,7 +44,7 @@ let rec insert char_list (Node (is_word, tree)) =
              char_list. *)
         else insert t empty
       in
-      Node (is_word, CharMap.add h new_tree tree)
+      Node (CharMap.add h new_tree tree)
 
 let rec prepend prefix lst =
   match lst with
@@ -73,23 +53,26 @@ let rec prepend prefix lst =
 
 (* For all keys in tree, continuting traversing through value pointed to by key
    if value != empty, else return key. *)
-let rec search_all (Node (is_word, tree)) =
+let rec search_all (Node tree) =
   let pairs = CharMap.bindings tree in
-  match pairs with
-  | [] -> []
-  | (key, _) :: t ->
-      if CharMap.cardinal (get_tree (CharMap.find key tree)) != 0 then
-        prepend (String.make 1 key) (search_all (CharMap.find key tree))
-      else [ String.make 1 key ]
+  print_endline (string_of_int (List.length pairs));
+  let rec search_pairs pairs =
+    match pairs with
+    | [] -> []
+    | (key, _) :: t ->
+        print_endline ("at " ^ String.make 1 key);
+        (* (String.make 1key :: *)
+        let a = search_pairs t in
+        if CharMap.cardinal (get_tree (CharMap.find key tree)) != 0 then
+          a @ prepend (String.make 1 key) (search_all (CharMap.find key tree))
+        else a @ [ String.make 1 key ]
+  in
+  search_pairs pairs
 
-let rec search prefix_list (Node (is_word, tree)) =
-  print_endline "hi";
+let rec search prefix_list (Node tree) =
   match prefix_list with
-  | [] -> if is_word then [] else search_all (Node (is_word, tree))
-  | h :: t ->
-      print_endline
-        ("at " ^ String.make 1 h ^ " " ^ string_of_int (CharMap.cardinal tree));
-      prepend (String.make 1 h) (search t (CharMap.find h tree))
+  | [] -> if is_empty tree then [] else search_all (Node tree)
+  | h :: t -> prepend (String.make 1 h) (search t (CharMap.find h tree))
 
 (* let search prefix tree = let prefix_list = to_char_list prefix in search_help
    prefix_list tree *)
--- test/test_group_proj.ml
+++ test/test_group_proj.ml
@@ -3,9 +3,11 @@ open Group_proj.Trie
 
 let tree = empty
 let tree = insert (to_char_list "apple") tree
-let tree = insert (to_char_list "dog") tree
-let tree = insert (to_char_list "triangle") tree
-let tree = insert (to_char_list "arson") tree
-let sear = search (to_char_list "a") tree
-let fold lst = List.fold_left (fun acc elem -> acc ^ elem) "" lst
+let tree = insert (to_char_list "aprle") tree
+let tree = insert (to_char_list "appol") tree
+let tree = insert (to_char_list "appla") tree
+let tree = insert (to_char_list "apole") tree
+let sear = search (to_char_list "") tree
+let fold lst = List.fold_left (fun acc elem -> elem ^ " " ^ acc) "" lst
 let _ = print_endline (fold sear)
+let _ = print_endline (string_of_int (List.length sear))
