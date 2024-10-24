ae90c2a067e931cc91968e0e80d92334faff0e4e
Kathy Chen <kc994@cornell.edu>
2024-10-24 18:54:22 -0400

    trying to make text bigger

--- lib/view.ml
+++ lib/view.ml
@@ -2,20 +2,20 @@ open Graphics
 
 let basic_window () =
   open_graph " 1920x1080";
+  set_color white;
   set_window_title "Auto_Complete";
 
-  set_color white;
+  set_text_size 500;
+
   fill_rect 0 0 800 600;
-
-  (* Wait for a key press to keep the window open *)
-  ignore (read_key ());
-
-  set_color blue;
   let title = "AutoComplete :)" in
+  set_text_size 100;
+  set_color blue;
   let x = (size_x () - (String.length title * 8)) / 2 in
-  let y = size_y () / 2 in
+  let y = 9 * size_y () / 10 in
   moveto x y;
   draw_string title;
 
-  (* Close the graphics window after key press *)
+  (* Wait for a key press to keep the window open *)
+  ignore (read_key ());
   close_graph ()
