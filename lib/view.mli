4897d96aab1decd5ffd88a3608d9df845a31504c
Kathy Chen <kc994@cornell.edu>
2024-10-25 02:45:10 -0400

    Working on view functionality and GUI. Overlapped words

--- lib/view.mli
+++ lib/view.mli
@@ -1,6 +1,8 @@
 type t
 
-val print_to_screen : unit -> 'a
+val print_to_screen : string -> unit
 val basic_window : unit -> unit
 val start_text : unit -> unit
-val return_suggest : int -> t -> int -> int -> unit
+val return_suggest : int -> int -> int -> unit
+val tree : t
+val accumulate_and_display : string -> 'a
