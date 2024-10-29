82583e55d62dfb95d10c0e36261c0a727707e4a8
Kathy Chen <kc994@cornell.edu>
2024-10-28 23:53:48 -0400

    Added Documentation attempted ppm image reading

--- lib/view.ml
+++ lib/view.ml
@@ -3,7 +3,7 @@ include Tree.Trie
 module Tr = Trie
 
 type t = Tr.t
-
+(**[insert_all lis t] inserts all words in lis into a t*)
 let rec insert_all word_list tree =
   match word_list with
   | [] -> tree
@@ -11,6 +11,7 @@ let rec insert_all word_list tree =
       let new_tree = Tr.insert (Tr.to_char_list h) tree in
       insert_all t new_tree
 
+(*test tree I am using for the prototype*)
 let tree =
   insert_all
     [
@@ -25,8 +26,10 @@ let tree =
     ]
     Tr.empty
 
+(**[string_to_char_list str] is the char list form a string*)
 let string_to_char_list str = List.of_seq (String.to_seq str)
 
+(**[blend_color alpha bg fg] blends together color bg and fg according to alpha*)
 let blend_color alpha bg fg =
   let r1 = (bg lsr 16) land 0xFF in
   (* Extract red component from bg *)
@@ -48,6 +51,7 @@ let blend_color alpha bg fg =
 
   rgb (blend alpha r1 r2) (blend alpha g1 g2) (blend alpha b1 b2)
 
+  (**[bubble st ed] draws the search bar bubble*)
 let rec bubble (st : int) (ed : int) () =
   set_color (rgb 229 228 226);
 
@@ -55,10 +59,7 @@ let rec bubble (st : int) (ed : int) () =
   fill_circle ed 540 20;
   fill_rect st 520 (ed - st) 40
 
-(* let rec print_to_screen () = (* let x = 100 in let y = 540 in moveto x y; *)
-   let c = read_key () in if c = '\027' then () else ( set_color black;
-   draw_string (String.make 1 c); flush (); print_to_screen ()) *)
-
+(**[no_suggest] clears the suggestions on the GUI*)
 let no_suggest () =
   set_color (rgb 229 228 226);
   (* Pure white color *)
@@ -68,6 +69,7 @@ let no_suggest () =
     800 (* Width matching print_suggestions1 *)
     240
 
+(**[words pos_y] prints the words in a vertical list*)
 let rec words pos_y = function
   | [] -> ()
   | h :: t ->
@@ -76,6 +78,7 @@ let rec words pos_y = function
       draw_string h;
       words (pos_y - 40) t
 
+(**[print_suggestions1 lst] words from lst onto a grey suggestion box*)
 let print_suggestions1 lst =
   let x_offset = (size_x () / 3) - 125 in
   let y_offset = 500 in
@@ -85,9 +88,11 @@ let print_suggestions1 lst =
   words (y_offset - 40) lst;
   synchronize ()
 
+(**[is_inside (x,y) (rect_x, rect_y) width height] is whether x,y is in rect_x,rect_y*)
 let is_inside (x, y) (rect_x, rect_y) width height =
   x >= rect_x && x <= rect_x + width && y >= rect_y && y <= rect_y + height
 
+(**[find_chosen_suggestion lst] is the suggestion that is clicked on*)
 let find_chosen_suggestion lst =
   let event = wait_next_event [ Button_down; Key_pressed ] in
   let click_x = event.mouse_x in
@@ -106,16 +111,7 @@ let find_chosen_suggestion lst =
       (List.mapi (fun i option -> (option, i)) lst)
   else None
 
-let continue lst =
-  let event = wait_next_event [ Key_pressed ] in
-  if
-    (not (List.is_empty lst))
-    && ((event.key >= 'a' && event.key <= 'z')
-       || (event.key >= 'A' && event.key <= 'Z'))
-  then print_suggestions1 lst
-  else no_suggest ();
-  moveto (size_x () / 3) 540
-
+(**[turn_char_to_string acc] string of a char that is read *)
 let rec turn_char_to_string acc =
   let c = read_key () in
   if c = ' ' then ()
@@ -123,40 +119,26 @@ let rec turn_char_to_string acc =
     let accum = acc ^ String.make 1 c in
     turn_char_to_string accum
 
-let helper_text x y =
-  let title = "" in
-  set_text_size 100;
-  set_color black;
-  moveto x y;
-  draw_string title
-
+(**[string_lis_to_string lis] is a string of a string list*)
 let string_lis_to_string lis = String.concat "" lis
 
+(**[turn_char_to_string_lis trie] is a list of the suggestions for a key pressed *)
 let turn_char_to_string_lis trie =
   let c = read_key () in
   let char_lis = c :: [] in
   Tr.search char_lis trie
 
-let print_suggestions lis x y =
-  let text = String.concat "" lis in
-  set_text_size 100;
-  set_color black;
-  moveto x y;
-  draw_string text
-
-let return_suggest incr x y =
-  let lis = turn_char_to_string_lis tree in
-  print_suggestions lis x (y + incr)
-
+  (**[autofill word_accum suggestions] rest of the word to be filled by the suggestion*)
 let autofill word_accum suggestions =
   let suggestion = List.nth suggestions 0 in
   String.sub suggestion (String.length word_accum)
     (String.length suggestion - String.length word_accum)
 
+(** [print_autofill rest_of_word x_int y_int color] prints the autofilled word from suggestions*)
 let rec print_autofill rest_of_word x_int y_int color =
   let bg = rgb 229 228 226 in
   let fg = color in
-  let col = blend_color 0.2 bg fg in
+  let col = blend_color 0.4 bg fg in
   let count = x_int + 7 in
   set_color col;
   moveto count y_int;
@@ -167,6 +149,7 @@ let rec print_autofill rest_of_word x_int y_int color =
       count y_int color
   else ()
 
+(**[basic_window ()] creates a blank GUI*)
 let basic_window () =
   open_graph " 1920x1080";
   bubble 80 1280 ();
@@ -194,6 +177,7 @@ let start_text () =
   moveto x y;
   draw_string title
 
+(**[accumulate_and_display acc] accumulates keys pressed and prints suggestions to screen*)
 let rec accumulate_and_display acc =
   let c = read_key () in
   let updated_acc = acc ^ String.make 1 c in
@@ -205,36 +189,14 @@ let rec accumulate_and_display acc =
   else (
     print_suggestions1 suggestions;
     accumulate_and_display updated_acc)
-
-(* let rec print_to_screen accum = (* Get the current character input *) let
-   event = wait_next_event [ Key_pressed ] in let c = event.key in
-
-   if c = '\027' then () else (* Append the character to the accumulator if it's
-   not a space *) let new_accum = if c = ' ' then "" else accum ^ String.make 1
-   c in let total_accum = accum ^ String.make 1 c in no_suggest ();
-
-   let suggestions = Tr.search (string_to_char_list new_accum) tree in let
-   choice, chose_word = if suggestions = [] then (* Call no_suggest if there are
-   no suggestions *) let () = no_suggest () in ("", false) else let () =
-   print_suggestions1 suggestions in let user_choice = find_chosen_suggestion
-   suggestions in match user_choice with | None -> ("", false) | Some s -> (s,
-   true) in
-
-   (* Display the current typed characters *) set_color black; let x_offset =
-   140 in let y_offset = 540 in let current_x = x_offset + (String.length
-   total_accum * 7) in let total_accum = if chose_word then total_accum ^ " " ^
-   choice else total_accum in moveto current_x y_offset; if String.length
-   new_accum > 0 then draw_string (String.make 1 total_accum.[String.length
-   total_accum - 1]) else if chose_word then draw_string (" " ^ choice) else
-   draw_string " "; (* Call the function recursively with the new accumulator *)
-   print_to_screen total_accum *)
-
+(**[print_word word x_int y_int] prints each word to a coordinate*)
 let print_word word x_int y_int =
   let len = String.length word in
   let nex_x = x_int + len + 1 in
   moveto nex_x y_int;
   draw_string word
 
+(**[print_to_screen accum x_int y_int counter] prints the suggestions and present typing to screen*)
 let rec print_to_screen accum x_int y_int counter =
   synchronize ();
   (* Get the current character input *)
@@ -257,7 +219,6 @@ let rec print_to_screen accum x_int y_int counter =
   let new_accum = if c = ' ' then "" else accum ^ String.make 1 c in
   let suggestions = Tr.search (string_to_char_list new_accum) tree in
   if c = ' ' then no_suggest () else print_suggestions1 suggestions;
-  print_endline new_accum;
 
   if new_accum = "" then no_suggest () else print_suggestions1 suggestions;
 
@@ -280,3 +241,61 @@ let rec print_to_screen accum x_int y_int counter =
 
   (* Call the function recursively with the new accumulator *)
   print_to_screen new_accum count y_offset (count + 4)
+
+  (**functions to read a ppm file to display a image*)
+
+  (**[skip_comments ic] doesn't read comments in a ppm file in the returned string*)
+  let skip_comments ic =
+    let rec aux () =
+      let line = input_line ic in
+      if String.length line > 0 && String.get line 0 = '#' then
+        aux ()  (* Skip comment lines *)
+      else
+        line
+    in
+    aux ()
+    
+    (**[load_ppm filename] turns the ppm into a image on screen*)
+    let load_ppm filename =
+      let ic = open_in filename in
+      (* Read the magic number (P3) *)
+      let magic_number = input_line ic in
+      if magic_number <> "P3" then failwith "Unsupported PPM format";
+    
+      (* Skip comments and read width, height *)
+      let line = skip_comments ic in
+      let (width, height) = Scanf.sscanf line "%d %d" (fun w h -> (w, h)) in
+    
+      (* Read the max color value (usually 255) *)
+      let _ = input_line ic in  (* We can ignore it *)
+    
+      (* Read all pixel data *)
+      let pixel_data = ref [] in
+     
+        while true do
+          let value = input_line ic in
+          pixel_data := value :: !pixel_data
+        done;
+     
+    
+      (* Check if we have enough values *)
+      let total_pixels = width * height * 3 in
+      if List.length !pixel_data < total_pixels then
+        failwith "Not enough pixel values in the file";
+    
+      (* Read pixel data and plot each pixel *)
+      let rec read_pixel index =
+        if index >= total_pixels then () (* End of pixel data *)
+        else
+          let r = int_of_string (List.nth !pixel_data index) in
+          let g = int_of_string (List.nth !pixel_data (index + 1)) in
+          let b = int_of_string (List.nth !pixel_data (index + 2)) in
+          let color = Graphics.rgb r g b in
+          Graphics.set_color color;            (* Set the color first *)
+          Graphics.plot (index mod width) (height - (index / width) - 1);
+          read_pixel (index + 3)
+      in
+    
+      read_pixel 0;
+    
+      close_in ic
